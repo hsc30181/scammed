562c4e6ae263cdee689339412278a516dd4c644f
jchs <s30181@asmadrid.es>
2025-10-03 13:07:53 +0200

    Update scamme.py

--- scamme.py
+++ scamme.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    print("Congratulations, Mr. Sissons! You have been tricked!")
-    return "Check your console log!"
+    return """Congratulations, Mr. Sissons. You have been tricked by me! hahaha
+    Credit @ Hyunsoo Cho, developer's comment: I made this myself when you weren't here hahaha
+    Powered by Render (Render.com)"""
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render gives this dynamically
     app.run(host="0.0.0.0", port=port, debug=False)
+

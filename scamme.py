c0912a44ae36ae8d01c21cd31518bc88e41e42bf
jchs <s30181@asmadrid.es>
2025-10-03 12:58:38 +0200

    Update scamme.py

--- scamme.py
+++ scamme.py
@@ -1,12 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    print("Congratulations, Mr. Sissons! You have been scammed!")
-    print("Credit @Jacob, developer's comment: I made this Mr. Sissons haha")
-    return "Press F12!(Don't worry. I made this myself and it's totally safe.)"
+    print("Congratulations, Mr. Sissons! You have been tricked!")
+    return "Check your console log!"
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))  # Render gives this dynamically
+    app.run(host="0.0.0.0", port=port, debug=False)

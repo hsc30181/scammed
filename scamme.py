688ab9bb4544efbc68fce88784b52382e94d80bb
jchs <s30181@asmadrid.es>
2025-10-07 15:11:14 +0200

    Update scamme.py

--- scamme.py
+++ scamme.py
@@ -1,15 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return """Congratulations, Mr. Sissons. You have been tricked by me! hahaha
-    Credit @ Hyunsoo Cho, developer's comment: I made this myself when you weren't here hahaha
-    Powered by Render (Render.com)"""
+    return render_template(index.html)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render gives this dynamically
     app.run(host="0.0.0.0", port=port, debug=False)
-

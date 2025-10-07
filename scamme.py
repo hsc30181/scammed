a642deebd5f1d04e54059bfab3580b2d82b300fa
jchs <s30181@asmadrid.es>
2025-10-07 15:19:00 +0200

    Update scamme.py

--- scamme.py
+++ scamme.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template(index.html)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render gives this dynamically
     app.run(host="0.0.0.0", port=port, debug=False)
+
